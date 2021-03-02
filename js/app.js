7776d291ef5077178267bf038f48d899b0fb157f
clockwerkz <clockwerkz@gmail.com>
2021-03-01 23:05:12 -0800

    adding touch events

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 const wrapper = document.querySelector(".horizontal-nav");
 const nav = document.querySelector(".nav");
 currentPosition = 0;
+startingTouchPosition = 0;
 
 wrapper.onwheel = handleMouseScrollWheel;
+wrapper.addEventListener("touchstart", (e)=>{
+    const { clientX } = e.touches[0];
+    startingTouchPosition = clientX;
+});
+wrapper.addEventListener("touchmove", (e)=>{
+    const { clientX, clientY } = e.touches[0];
+    console.log(clientX, clientY);
+    currentPosition += (clientX - startingTouchPosition) * .05;
+    nav.style.transform = `translateX(${currentPosition}px)`;
+});
 
 document.querySelector(".work__title").addEventListener("click", function(){
     currentPosition = 0;
